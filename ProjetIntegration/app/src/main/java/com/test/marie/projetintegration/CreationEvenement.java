d5ea103499892bbdc07716122adbeea7bf3b8b5d
kiwi0511 <marie_heinen@hotmail.com>
2015-10-02 09:32:29 +0200

    Menu terminé+passer d'une activité à une autre

---
+++ ProjetIntegration/app/src/main/java/com/test/marie/projetintegration/CreationEvenement.java
@@ -0,0 +1,114 @@
+package com.test.marie.projetintegration;
+/**
+ * Created by martin on 30-09-15.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+
+
+
+public class CreationEvenement extends Activity {
+    // La chaîne de caractères de champVide
+    private final String champVide = "Vous devez compléter tous les champs";
+
+    Button suivant = null;
+    Button raz = null;
+
+    TextView messageError =null;
+
+    EditText nomEvent = null;
+    EditText motDePasse = null;
+    EditText nombreEtape = null;
+
+    RadioGroup radioGroup = null;
+    RadioButton radioPublic =null;
+    RadioButton radioPrivee =null;
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.creation_evenement);
+
+        // On récupère toutes les vues dont on a besoin
+        suivant = (Button)findViewById(R.id.suivant);
+        raz = (Button)findViewById(R.id.raz);
+
+        nomEvent = (EditText)findViewById(R.id.nomEvent);
+        motDePasse = (EditText)findViewById(R.id.motDePasse);
+        nombreEtape = (EditText)findViewById(R.id.nombreEtape);
+
+        messageError =(TextView)findViewById(R.id.messageError);
+
+        radioGroup = (RadioGroup)findViewById(R.id.group);
+        radioPublic = (RadioButton)findViewById(R.id.radio1);
+        radioPrivee = (RadioButton)findViewById(R.id.radio2);
+
+        // On attribue un listener adapté aux vues qui en ont besoin
+        suivant.setOnClickListener(suivantListener);
+        raz.setOnClickListener(razListener);
+        radioGroup.setOnClickListener(radioListener);
+        radioPublic.setOnClickListener(radioListener);
+        radioPrivee.setOnClickListener(radioListener);
+    }
+
+    // Uniquement pour le bouton "suivant"
+    private OnClickListener suivantListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(nomEvent.getText().length() == 0){
+                messageError.setText(champVide);
+            }
+            else if(nombreEtape.getText().length() == 0){
+                messageError.setText(champVide);
+            }
+            else{
+                messageError.setText("ca fonctionne plus qu'a trouvé cmt chnagé d'activity");
+                //Changer d'activity mais comment ?
+            }
+        }
+    };
+
+    // Listener du bouton de remise à zéro
+    private OnClickListener razListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            nomEvent.getText().clear();
+            motDePasse.getText().clear();
+            nombreEtape.getText().clear();
+        }
+    };
+
+    private OnClickListener radioListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            boolean publique = false;
+            // On récupère l'identifiant du bouton qui est coché
+            int id = radioGroup.getCheckedRadioButtonId();
+            System.out.println(id);
+
+            switch (v.getId()){
+
+                case R.id.radio1: publique = true; break;
+                case R.id.radio2: publique = false; break;
+            }
+
+            if(publique){
+                motDePasse.setVisibility(View.INVISIBLE);
+            }
+            else{
+                motDePasse.setVisibility(View.VISIBLE);
+            }
+        }
+    };
+
+}
--- ProjetIntegration/app/src/main/java/com/test/marie/projetintegration/MainActivity.java
+++ ProjetIntegration/app/src/main/java/com/test/marie/projetintegration/MainActivity.java
@@ -2,26 +2,26 @@ package com.test.marie.projetintegration;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.*;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void VersProfil(View view) {
+        setContentView(R.layout.activity_gestion_du_profil);
     }
-
-    public void sendMessage(View view) {
-        System.out.println("Hello");
+    public void VersEvent(View view) {
+        setContentView(R.layout.creation_evenement);
     }
 
     @Override
